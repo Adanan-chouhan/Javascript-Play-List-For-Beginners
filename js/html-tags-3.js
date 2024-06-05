1a3402a85a1474a85d016592fad596e8b704b965
adnan chouhan <adananchouhan@gmail.com>
2024-06-05 15:52:41 +0530

    javascript beginer

---
+++ js/html-tags-3.js
@@ -0,0 +1,4 @@
+document.write("<h1>Adanan chouhan</h1><br>")
+document.write("<h2>Adanan chouhan</h2><br>")
+document.write("<h3>Adanan chouhan</h3><br>")
+document.write("<h4>Adanan chouhan</h4><br>")
